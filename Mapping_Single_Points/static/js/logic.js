a7ca4c3e98c25c060f68505457681a57bebd72af
robyndook <cook.robync@gmail.com>
2022-03-20 23:25:06 -0700

    update Files

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,3 +1,8 @@
-var marker = L.marker([51.5, -0.09]).addTo(map);
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Add console.log to check to see if our code is working.
+console.log("working");
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([34.0522, -1188.2437], 14);
+// Add a marker to the map for Los Angeles CaliforniaS
+L.circle([34.0522, -118.2437], {
+    radius: 100
+ }).addTo(map);
