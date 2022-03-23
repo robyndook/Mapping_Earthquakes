0587a63f76bbee1aad8e5354a7dd9eb8f2d40341
robyndook <cook.robync@gmail.com>
2022-03-22 20:30:06 -0700

    Delete Module Files

--- Mapping_Single_Points/static/js/logic.js
+++
@@ -1,8 +0,0 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -1188.2437], 14);
-// Add a marker to the map for Los Angeles CaliforniaS
-L.circle([34.0522, -118.2437], {
-    radius: 100
- }).addTo(map);
